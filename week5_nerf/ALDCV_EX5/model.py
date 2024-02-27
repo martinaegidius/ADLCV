93066cb4356ac2e651d076cc812d5456c61a1e2c
martinaegidius <gygridd@gmail.com>
2024-02-27 15:30:21 +0100

    fixed foward

--- week5_nerf/ALDCV_EX5/model.py
+++ week5_nerf/ALDCV_EX5/model.py
@@ -13,7 +13,7 @@ class NeRF(nn.Module):
         self.n_layers = n_layers
         self.hidden_dim = hidden_dim
         self.d_input = d_input
-        self.skips = skips
+        self.skips = skips #is depth at which we re-concat gamma(x)
         self.d_viewdirs = d_viewdirs
         
         self.pts_linears = nn.ModuleList(
@@ -22,7 +22,7 @@ class NeRF(nn.Module):
         )
         
         ### Implementation according to the official code release (https://github.com/bmild/nerf/blob/master/run_nerf_helpers.py#L104-L105)
-        if d_viewdirs is not None:
+        if d_viewdirs is not None:  #positional encoding of the input viewing direction gamma(d)
             self.views_linears = nn.ModuleList([nn.Linear(d_viewdirs + hidden_dim, hidden_dim//2)])
         
         if d_viewdirs is not None:
@@ -42,24 +42,31 @@ class NeRF(nn.Module):
         input_views = viewdirs
 
         h = input_pts
+        m = nn.ReLU()
         # for each layer with index i
         for i, l in enumerate(self.pts_linears):
-            h = ... # HINT: feed h to the layer i and rewrite to h
-            h = ... # HINT: use relu
+            h = l(h) # HINT: feed h to the layer i and rewrite to h
+            h = m(h) # HINT: use relu
             if i in self.skips:
-                h = ... # implement skip with torch.cat
+                #print("want to cat the following two")
+                #print(h.shape)
+                #print(input_pts.shape)
+                h = torch.cat((h,input_pts),dim=1) # implement skip with torch.cat
 
-        if self.d_viewdirs is not None:
-            alpha = ... # HINT: feed h to alpha linear
-            feature = ... # HINT: feed h to feature linear
-            h = ... # HINT: concat feature and input_views to create the input for the views_linreas
+        if self.d_viewdirs is not None: # positional encoding of the input viewing direction gamma(d)
+            alpha = self.alpha_linear(h) # HINT: feed h to alpha linear
+            feature = self.feature_linear(h) # HINT: feed h to feature linear
+            #print("1. now want cat ",alpha.shape,feature.shape)
+            
+            h = torch.cat((feature,input_views),dim=1) # HINT: concat feature and input_views to create the input for the views_linears
         
             for i, l in enumerate(self.views_linears):
-                h = ... # HINT: forward for views_linears of i
-                h = ... # HINT: Use relu
+                h = l(h) # HINT: forward for views_linears of i
+                h = m(h) # HINT: Use relu
 
-            rgb = ... # HINT: calculate rgb values with rgb_layer
-            outputs = ... # HINT: concat rgb and alpha
+            rgb = self.rgb_linear(h) # HINT: calculate rgb values with rgb_layer
+            #print("2. now want cat ",rgb.shape,alpha.shape)
+            outputs = torch.cat((rgb,alpha),dim=1) # HINT: concat rgb and alpha
         else:
             outputs = self.output_linear(h)
 
